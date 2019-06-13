9eb6952c7ee60aeca3be9c0c9c0ebe7f0c61fbd9
biroa <adam.biro+sourcelair@gmail.com>
2019-06-13 15:11:32 +0000

    all functionalities

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.templatetags.static import static
 # Importing Google spreadsheets aka Airtable DB on steroids ...
 from airtable import Airtable
 # Importing os to handle environment variables
@@ -13,7 +14,6 @@ AT = Airtable(os.environ.get('AIRTABLE_MOVIESTABLE_BASE_ID'),
 
 # Create your views here.
 def home_page(request):
-    #print(str(request.GET.get('query','')))
     user_query = str(request.GET.get('query',''));
     search_result = AT.get_all(formula="FIND('" + user_query.lower() + "', LOWER( {Name} ) )")
     # contex dictionary
@@ -26,11 +26,21 @@ def create(request):
     if request.method == 'POST':
         data = {
             'Name': request.POST.get('name'),
-            'Pictures': [{ 'url': request.POST.get('url')}],
+            'Pictures': [{ 'url': request.POST.get('url') or 'https://www.quantabiodesign.com/wp-content/uploads/No-Photo-Available-768x960.jpg'}],
             'Rating': int(request.POST.get('rating')),
             'Notes': request.POST.get('notes'),
         }
-        AT.insert(data)
+        try:
+            response = AT.insert(data)
+            # --== Notify on Create ==--
+            # Dictionaries have a function get ... we can retrive information from
+            # the dict without braking the code ... So if the key does not exist in
+            # the dict it returns null without it it would break
+            # {data in format func will be placed here}
+            messages.success(request,'New Movie Added : {}'.format(response['fields'].get('Name')))
+        except Exception as e:
+            # If something went wrong show me an error without crashing the app
+            messages.warning(request, 'Error while trying to create a new movie: {}' . format(e))
 
     return redirect('/')
 
@@ -39,10 +49,31 @@ def edit(request, movie_id):
     if request.method == 'POST':
         data = {
             'Name': request.POST.get('name'),
-            'Pictures': [{'url': request.POST.get('url')}],
+            'Pictures': [{'url': request.POST.get('url') or 'https://www.quantabiodesign.com/wp-content/uploads/No-Photo-Available-768x960.jpg'}],
             'Rating': int(request.POST.get('rating')),
             'Notes': request.POST.get('notes'),
         }
-        AT.update(movie_id, data);
+
+        # Error Handling ....
+
+        try: # try this
+            response = AT.update(movie_id, data);
+            # Notify on Edit / Update
+            messages.warning(request,'The edited movie is {}'.format(response['fields'].get('Name')))
+        except Exception as e:
+            # If something went wrong show me an error without crashing the app
+            messages.warning(request, 'Error while trying to edit a movie: {}' . format(e))
+
+    return redirect('/')
+
+#app.com/delete/rec0wPWwWIm0FpaJw
+def delete(request, movie_id):
+    try:
+        # Notify on Delete
+        messages.error(request,'Deleted Movie : {}'.format(AT.get(movie_id)['fields'].get('Name')), extra_tags='danger')
+        AT.delete(movie_id)
+    except Exception as e:
+        # If something went wrong show me an error without crashing the app
+        messages.warning(request, 'Error while trying to delete a movie: {}' . format(e))
 
     return redirect('/')
